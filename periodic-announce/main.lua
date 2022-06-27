90869489633160649dfbfec3dba5da2e20ea5b21
Lion Kortlepel <development@kortlepel.com>
2022-06-27 03:38:32 +0200

    add periodic announce plugin

---
+++ periodic-announce/main.lua
@@ -0,0 +1,15 @@
+-- Copyright (c) 2022 Lion Kortlepel. Use allowed under MIT license.
+
+local minutes = 5
+local message = "this is lionkor's periodic reminder plugin"
+
+function discordReminder()
+	MP.SendChatMessage(-1, message)
+end
+
+MP.CancelEventTimer("nMinTimer")
+
+MP.CreateEventTimer("nMinTimer", 1000 * 60 * minutes)
+
+MP.RegisterEvent("nMinTimer", "discordReminder")
+
